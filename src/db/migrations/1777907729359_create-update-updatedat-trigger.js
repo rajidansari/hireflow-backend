840af5a363275f3869deeea7fcd5812cf4713aac
Rajid Ansari <rajidansari25@gmail.com>
2026-05-04 17:21:08 +0000

    db: create updated_at trigger func

---
+++ src/db/migrations/1777907729359_create-update-updatedat-trigger.js
@@ -0,0 +1,32 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+	pgm.sql(`
+			CREATE OR REPLACE FUNCTION update_updated_at()
+			RETURNS TRIGGER AS $$
+			BEGIN
+				NEW.updated_at = NOW();
+				RETURN NEW;
+			END;
+			$$ LANGUAGE plpgsql;
+		`);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+	pgm.sql(`
+			DROP FUNCTION IF EXISTS update_updated_at;
+		`)
+};
