5ffad03f1c3be1e458745b7923186007818231bb
Rajid Ansari <rajidansari25@gmail.com>
2026-05-04 11:01:26 +0000

    db: add enum types

--- src/db/migrations/1777882650559_create-enums-types.js
+++ src/db/migrations/1777882650559_create-enums-types.js
@@ -8,11 +8,23 @@ export const shorthands = undefined;
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {};
+export const up = (pgm) => {
+	pgm.sql(`
+			CREATE TYPE user_role AS ENUM('candidate', 'employer');
+			CREATE TYPE job_status AS ENUM('draft', 'active', 'expired');
+			CREATE TYPE app_status AS ENUM('pending', 'reviewed', 'hired', 'rejected');
+		`)
+};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {};
+export const down = (pgm) => {
+	pgm.sql(`
+			DROP TYPE user_role IF EXISTS;
+			DROP TYPE job_status IF EXISTS;
+			DROP TYPE app_status IF EXISTS;
+		`)
+};
