3e0b180dc616c16121d374ef427c226c354972e5
Rajid Ansari <rajidansari25@gmail.com>
2026-05-08 17:10:12 +0000

    add auth middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+import { validateAccessToken } from '../utils/jwt.utils.js';
+
+function auth(req, res, next) {
+  try {
+		const accessToken = req.headers?.['authorization']?.split(' ')[1];
+		
+		  if (!accessToken) {
+		    return res.status(401).json({ message: 'Unauthorized access denied' });
+		  }
+		
+		  const decoded = validateAccessToken(accessToken);
+			if(!decoded) {
+				return res.status(403).json({message: "Invalid token"});
+			}
+		
+			req.user = decoded;
+		
+			next();
+	} catch (err) {
+		console.log(`Auth middleware failed :: ${err}`);
+		if(err.name === "JsonWebTokenError") {
+			return res.status(401).json({message: "Invalid token"})
+		}
+		res.status(500).json({message: "Internal server error"})
+	}
+
+}
+
+
+export default auth;
