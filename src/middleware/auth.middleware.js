8843c4722c3f1971375e1e6a7916182e4508db4e
Rajid Ansari <rajidansari25@gmail.com>
2026-05-08 17:28:07 +0000

    refactor auth middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -21,6 +21,9 @@ function auth(req, res, next) {
 		if(err.name === "JsonWebTokenError") {
 			return res.status(401).json({message: "Invalid token"})
 		}
+		if(err.name === "TokenExpiredError") {
+			return res.status(401).json({message: "Token expired"});
+		}
 		res.status(500).json({message: "Internal server error"})
 	}
 
